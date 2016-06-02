6f39827985f711a6db3cb9a351631493b8315c83
paveltsytovich <pavel.tsytovich@gmail.com>
2016-06-02 10:53:24 +0300

    If to polymorhism before refactoring

---
+++ net/LiveDemo/If2Polymorph/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If2Polymorph
+{
+    class Employee
+    {
+        public int Workhours {get;set;}
+       public int GetBonus(int yearOfJob)
+        {
+            int result=0;
+            switch(typeOfEmployee)
+            {
+                case 1:
+                     result = Workhours * 10;
+                     break;
+                case 2:
+                     result = Workhours * 5 + 2;
+                     break;
+                case 3:
+                     result = Workhours * 10 / 2;
+                     break;                     
+            }
+           result += Workhours*2 * typeOfEmployee;
+           return result;
+        }
+    }
+}
---
+++ net/LiveDemo/If2Polymorph/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If2Polymorph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Ivan = new Employee() { Workhours = 5};
+            Console.WriteLine(Ivan.GetBonus(1));
+            Console.WriteLine(Ivan.GetBonus(2));
+            Console.WriteLine(Ivan.GetBonus(3));
+        }
+    }
+}
